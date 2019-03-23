08660732fc4e0ea3191a192235432c3fbdf6db7d
Piotr Mularski <piomul6473@gmail.com>
2019-03-23 14:09:19 +0100

    created fileanalyse route with filesize validation

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-'use strict';
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const app = express();
 
-var express = require('express');
-var cors = require('cors');
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+});
 
-// require and use "multer"...
-
-var app = express();
+// instance of multer upload here because of how error handling in multer is shown in documentation
+const upload = multer({
+	storage: storage,
+	limits: { fileSize: (5 * 1024 * 1024) }
+}).single('upfile');
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,8 +31,25 @@ app.get('/hello', function (req, res) {
 	res.json({ greetings: "Hello, API" });
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+	upload(req, res, (err) => {
+		if(err && err.code === 'LIMIT_FILE_SIZE') {
+			res.json({ error: 'File size too large. Maximum file Size is 5 Megabytes' });
+		} else if(err) {
+			res.json({ error: 'Cannot upload the file' });
+		} else {
+			const resObject = {
+				name: req.file.originalname,
+				size: req.file.size,
+				type: req.file.mimetype
+			}
+			res.json(resObject);
+		}
+	});
+});
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
